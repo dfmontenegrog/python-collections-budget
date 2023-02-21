52ae4e97820c45653db68f71b03c6d804b5c4ee8
dfmontenegrog <dfmontenegrog@gmail.com>
2023-02-20 21:41:56 -0500

    Completed module

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -14,7 +14,7 @@ spending_counter = collections.Counter(spending_categories)
 top5 = spending_counter.most_common(5)
 categories, count = zip(*top5)
 
-fig, ax = plt.subplots()
+fix, ax = plt.subplots()
 ax.bar(categories, count)
 ax.set_title('# of Purchases by Cathegory')
 plt.show()
