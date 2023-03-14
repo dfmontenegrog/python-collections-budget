2a54745c1f33c3a259dafcc54571984175db58e2
dfmontenegrog <dfmontenegrog@gmail.com>
2023-03-13 20:42:32 -0500

    Completed module

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -19,6 +19,9 @@ class BudgetList():
     def __len__(self):
         return len(self.expenses) + len(self.overages)
 
+    def __iter__(self):
+        pass
+
 def main():
     myBudgetList = BudgetList(1200)
     expenses = Expense.Expenses()
